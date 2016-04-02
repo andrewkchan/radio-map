f10c7ae625e75d26f5eab83633b9a90d0f175761
ninhja <ariannaninh@berkeley.edu>
2016-04-02 08:14:59 -0700

    flask file

---
+++ flaaasks/static/app.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+	$('.roadtrip').fadeOut(0);
+	$('#landing-hits').click(function() {
+		$('.landing').hide();
+		$('.roadtrip').show();
+		$('html').css('background-color', 'white');
+		$('body').css('background-color', 'white');
+	});
+	$('h1').click(function() {
+		('.roadtrip').hide();
+		('.landing').show();
+		$('html').css('background-color', '#27ae60');
+		$('body').css('background-color', '#27ae60');
+	})
+});
+
+function showRoadtrip() {
+
+}
+
+// var screenHeight = $(window).height(); 
+// var row = $('.row');
+
+// // Assign that height to the .row
+// row.css({
+//     'height': screenHeight + 'px',
+// });
+
+// // This makes the div's height responsive when you resize the screen or the window of the browser.
+// $(window).resize(function () {
+//     screenHeight = $(window).height();
+//     row.css({
+//         'height': screenHeight + 'px',
+//     });
+// });
